d564de0522eeb32352d21515b9a78676fb241bca
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-02-03 17:24:08 +0530

    init frontend

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+import "@/styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export const metadata: Metadata = {
+  title: "MarkaGames",
+  description: "In Game Asset Marketplace for Starknet",
+};
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300;400;900&family=PT+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Tektur:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
