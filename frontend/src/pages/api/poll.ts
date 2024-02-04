d0ed0b7fd170e0a23eb0761a3a455c85e0386789
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-02-04 21:15:36 +0530

    add title and og image

--- frontend/src/pages/api/poll.ts
+++ frontend/src/pages/api/poll.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Handle the request and send the response
-  let imageUrl = "https://picsum.photos/200/300";
+  let imageUrl = "https://picsum.photos/400/200";
 
   res.status(200).send(
     `
@@ -12,15 +12,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       <html>
         <head>
           <title>Vote Recorded</title>
+          <meta property="og:title" content="Vote Recorded">
+          <meta property="og:image" content="${imageUrl}">
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content=${imageUrl} />
           <meta property="fc:frame:button:1" content="Green" />
           <meta property="fc:frame:button:2" content="Purple" />
           <meta property="fc:frame:button:3" content="Red" />
-          <meta property="fc:frame:button:4" content="Blue" />
-        </head>
+          <meta property="fc:frame:button:4" content="Blue" /> 
+        </head> 
         <body>
-          <p>hello</p>
+          <p>THis is a very big body I assume Just seeing if it is working</p>
         </body>
       </html>`
   );
