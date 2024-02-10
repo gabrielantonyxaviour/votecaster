64e33823dd1a6f307f75d8367a44e9422b2a8110
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-02-10 16:12:22 +0530

    ran into farcaster error once again :(

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,14 +1,10 @@
-import * as React from "react";
 import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return <body>{children}</body>;
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,34 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
+import "@farcaster/auth-kit/styles.css";
+
+import Head from "next/head";
+import { useSession, signIn, signOut, getCsrfToken } from "next-auth/react";
+import {
+  SignInButton,
+  AuthKitProvider,
+  StatusAPIResponse,
+} from "@farcaster/auth-kit";
+import { useCallback, useState } from "react";
+const config = {
+  rpcUrl: "https://mainnet.optimism.io",
+  domain: "https://priv-cast.vercel.app",
+  siweUri: "https://priv-cast.vercel.app/login",
+};
 
 export default function Home() {
   return (
-    <div className="max-w-[1200px] mx-auto h-screen py-8">
-      <Navbar />
-    </div>
+    <>
+      <Head>
+        <title>Farcaster AuthKit + NextAuth Demo</title>
+      </Head>
+      <main style={{ fontFamily: "Inter, sans-serif" }}>
+        <AuthKitProvider config={config}>
+          <div className="max-w-[1200px] mx-auto h-screen py-8">
+            <Navbar />
+          </div>
+        </AuthKitProvider>
+      </main>
+    </>
   );
 }
