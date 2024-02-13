9ea72c52d6f70986df2fc7dd8bd237962e524b59
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-02-13 19:01:51 +0530

    m to p landing

--- app/src/components/Landing.tsx
+++ app/src/components/Landing.tsx
@@ -17,33 +17,35 @@ export default function Landing() {
   const content =
     "Cast polls in farcaster where the users can vote without revealing their identity. The sybil resistant polls ensure that the polls are not manipulated by fake votes.";
   return (
-    <div className="w-full rounded-xl bg-[#FBF6FF] text-[#450C63] px-20 pt-20   mt-4 ">
-      <div className="flex">
-        <div className="flex flex-col space-y-4 justify-between w-[50%] ">
-          <p className="w-[90%] text-5xl font-semibold">{hero}</p>
-          <p className="text-xl">{content}</p>
-          <div className="flex ml-2">
-            <Button
-              text="Get Started"
-              click={() => {
-                router.push("/polls");
-              }}
+    <div className="py-3">
+      <div className="w-full rounded-xl bg-[#FBF6FF] text-[#450C63] px-20 pt-20  ">
+        <div className="flex">
+          <div className="flex flex-col space-y-4 justify-between w-[50%] ">
+            <p className="w-[90%] text-5xl font-semibold">{hero}</p>
+            <p className="text-xl">{content}</p>
+            <div className="flex ml-2">
+              <Button
+                text="Get Started"
+                click={() => {
+                  router.push("/polls");
+                }}
+              />
+            </div>
+          </div>
+          <div className="w-[50%] flex flex-col justify-between">
+            <Image
+              src={"/hero.png"}
+              width={350}
+              height={350}
+              alt="hero"
+              className="mx-auto my-auto"
             />
           </div>
         </div>
-        <div className="w-[50%] flex flex-col justify-between">
-          <Image
-            src={"/hero.png"}
-            width={350}
-            height={350}
-            alt="hero"
-            className="mx-auto my-auto"
-          />
-        </div>
+        <p className="text-md text-center my-2 font-normal text-[#9508BF] pt-14 pb-4">
+          Built with 💜 by gabrielaxy.eth
+        </p>
       </div>
-      <p className="text-md text-center my-2 font-normal text-[#9508BF] pt-14 pb-4">
-        Built with 💜 by gabrielaxy.eth
-      </p>
     </div>
   );
 }
