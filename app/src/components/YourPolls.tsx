1f86d87f0668401d629be638b69f04a508ff4ad5
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-02-18 03:27:54 +0530

    fetched polls by creator

--- app/src/components/YourPolls.tsx
+++ app/src/components/YourPolls.tsx
@@ -6,9 +6,10 @@ import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { Data, QueryResponse } from "@/utils/airstackInterface";
 import { useQuery } from "@airstack/airstack-react";
+import getPollsByCreator from "@/utils/supabase/getPollsByCreator";
 
 export default function YourPolls() {
-  const polls = [];
+  const [polls, setPolls] = useState<any>([]);
   const router = useRouter();
   const [hasProfile, setHasProfile] = useState(false);
   const { address } = useAccount();
@@ -31,16 +32,21 @@ export default function YourPolls() {
   );
 
   useEffect(() => {
-    if (
-      data != null &&
-      (data as any).Socials != null &&
-      (data as any).Socials.Social != null &&
-      (data as any).Socials.Social.length > 0
-    ) {
-      setHasProfile(true);
-    } else {
-      setHasProfile(false);
-    }
+    (async function () {
+      if (
+        data != null &&
+        (data as any).Socials != null &&
+        (data as any).Socials.Social != null &&
+        (data as any).Socials.Social.length > 0
+      ) {
+        setHasProfile(true);
+        const polls = await getPollsByCreator({ creator: address as string });
+        console.log(polls);
+        setPolls(polls);
+      } else {
+        setHasProfile(false);
+      }
+    })();
   }, [data, loading, queryError]);
 
   useEffect(() => {
