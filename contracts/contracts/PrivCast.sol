8642dbf4ebd2808e2bcc68eaae0a9cb58b959c23
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-02-26 13:04:50 -0700

    add encrypted vote

--- contracts/contracts/PrivCast.sol
+++ contracts/contracts/PrivCast.sol
@@ -9,17 +9,17 @@ contract PrivCast{
     struct Poll{
         uint256 id;
         string pollUri;
-        uint256[4] votes;
+        mapping(uint256=>bool) hasVoted;
         uint256 createdTimestamp;
         uint256 validityDuration;
+        bytes[] encryptedVotes;
+        uint256 voteCount;
         bool isExists;
     }
 
-    mapping(uint256=>mapping(uint256=>bool)) public farcasterNullifiers; // pollId => nullifier => hasVoted
     mapping(uint256=>Poll) public polls; // pollId => poll
 
     uint256 public pollIdCounter;
-    address public anonAadhaarVerifierAddr;
     UltraVerifier public farcasterVerifier;
 
     constructor() {
@@ -28,28 +28,37 @@ contract PrivCast{
     }
 
     event PollCreated(uint256 pollId, address creatorAddress, string pollUri,uint256 createdTimestamp, uint256 validityDuration);
-    event VoteCast(uint256 pollId, uint256 nullifierHash,uint256 optionId);
+    event VoteCast(uint256 pollId, uint256 nullifierHash,bytes encryptedVote);
 
     function createPoll(string memory pollUri, uint256 validityDuration) public  {
-        polls[pollIdCounter]=Poll(pollIdCounter,pollUri, [uint256(0),uint256(0),uint256(0),uint256(0)],block.timestamp,validityDuration,true);
+        Poll storage poll=polls[pollIdCounter];
+        poll.id=pollIdCounter;
+        poll.createdTimestamp=block.timestamp;
+        poll.validityDuration=validityDuration;
+        poll.isExists=true;
+        poll.pollUri=pollUri;
         emit PollCreated(pollIdCounter, msg.sender, pollUri, block.timestamp, validityDuration);
         pollIdCounter++;    
     }
 
-    function castVote(bytes memory proof, uint256 pollId, uint256 vote, uint256 nullifierHash) public {
-        require(polls[pollId].isExists,"Poll does not exist");
+    function castVote(bytes memory proof, uint256 pollId, bytes calldata encryptedVote, uint256 nullifierHash) public {
+        Poll storage poll = polls[pollId];
+
+        require(poll.isExists,"Poll does not exist");
+        require(!poll.hasVoted[nullifierHash],"Already voted");
+        require(verifyFarcasterProof(proof, pollId, keccak256(encryptedVote), nullifierHash),"Invalid proof");
         
-        require(!farcasterNullifiers[pollId][nullifierHash],"Vote already casted");
-        require(verifyFarcasterProof(proof, pollId, vote, nullifierHash),"Invalid proof");
-        
-        polls[pollId].votes[vote]++;
-        emit VoteCast(pollId, nullifierHash, vote);
+        poll.encryptedVotes.push(encryptedVote);
+        poll.voteCount++;
+        poll.hasVoted[nullifierHash]=true;
+
+        emit VoteCast(pollId, nullifierHash, encryptedVote);
     }
 
-    function verifyFarcasterProof(bytes memory proof,uint256 pollId, uint256 _vote, uint256 _nullifierHash) public view returns(bool){
+    function verifyFarcasterProof(bytes memory proof,uint256 pollId, bytes32 _vote, uint256 _nullifierHash) public view returns(bool){
         bytes32[] memory publicInputs = new bytes32[](3);
         publicInputs[0] = bytes32(pollId);
-        publicInputs[1] = bytes32(_vote);
+        publicInputs[1] = _vote;
         publicInputs[2] = bytes32(_nullifierHash);
         
         try farcasterVerifier.verify(proof, publicInputs)
