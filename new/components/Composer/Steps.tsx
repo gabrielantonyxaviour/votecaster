268330fd89290d8d0fe378404a609ac67f54bcd5
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-08-19 22:43:31 +0530

    laoder done

--- new/components/Composer/Steps.tsx
+++ new/components/Composer/Steps.tsx
@@ -1,8 +1,8 @@
 export default function Steps({ step }: { step: number }) {
   return (
-    <div className="flex h-[7px] w-[80%] mx-auto">
-      {new Array(3).map((_, i) => (
-        <div className="w-[100px] space-x-2 h-[7px] bg-red-500"></div>
+    <div className="flex h-[7px] w-[80%] mx-auto border-2 border-[#450C63]">
+      {Array.from({ length: step }).map((_, index) => (
+        <div className={`w-[25%] space-x-2  bg-[#450C63] `}></div>
       ))}
     </div>
   );
